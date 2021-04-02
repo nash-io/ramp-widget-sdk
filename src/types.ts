ab8b897e4bf6cb5719219e2e19defdfb1cc45673
Mateus Ferreira <mateus@dlbn.co>
2021-04-02 14:33:16 -0300

    chore: remove unused code

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,1 @@
-export interface FiatRampRate {
-  name: string;
-  minPurchaseAmount: {
-    amount: number;
-    currency: string;
-  };
-  purchaseFiatPrice: {
-    price: {
-      amount: Number;
-      currencyA: string;
-      currencyB: string;
-    };
-  };
-}
-
 export type WidgetEnvironment = "LOCAL" | "PRODUCTION";
