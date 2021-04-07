421ea4631f06a55b0cf43367eaa5c3679d47865e
Mateus Ferreira <mateus@dlbn.co>
2021-04-07 15:14:04 -0300

    docs(example): tweak styles

--- example/with-react/src/App.js
+++ example/with-react/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import NashRampWidget from "./NashRampWidget";
 
@@ -11,20 +10,6 @@ function App() {
         target="aave"
         destination="0x0000000000000000000000000000000000000000"
       />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
