ee8ad03da2e7877aeb3b918dcb163409770a2cc9
Mateus Ferreira <mateus@dlbn.co>
2021-04-07 14:38:00 -0300

    fix: fix close button query selector

--- src/main.ts
+++ src/main.ts
@@ -94,7 +94,9 @@ export default class NashRamp {
           : ""
       }<iframe style="border:0;" src=${iframeUrl} width="100%" height="100%" /></div>`;
       if (options.onClose != null) {
-        const closeButton = document.querySelector(`[data-nash-fiat-ramp]`);
+        const closeButton = document.querySelector(
+          `[data-nash-fiat-ramp-widget-close-button]`
+        );
         closeButton?.addEventListener("click", options.onClose);
       }
     }
