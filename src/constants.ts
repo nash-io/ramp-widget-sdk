066aa319f32f20463501eb79cb90ff1fb3d810dc
Mateus Ferreira <mateus@dlbn.co>
2021-04-15 17:57:50 -0300

    feat(constants): declare constants for DOM selectors and mobile breakpoint

--- src/constants.ts
+++ src/constants.ts
@@ -9,3 +9,17 @@ export const envs: Record<WidgetEnvironment, string> = {
   LOCAL: "http://localhost:3000",
   PRODUCTION: "https://buy.nash.io",
 };
+
+export const MODAL_ID = "nash-ramp-modal";
+
+export const CLOSE_BUTTON_ID = "nash-fiat-ramp-widget-close-button";
+
+export const IFRAME_WRAPPER_ID = "nash-fiat-ramp-widget-iframe-wrapper";
+
+export const IFRAME_ID = "nash-fiat-ramp-widget-iframe";
+
+export const TARGET_ELEMENT_DATA_ATTR = "data-nash-fiat-ramp-widget";
+
+export const BODY_MODAL_OPEN_CLASS_NAME = "nash-fiat-ramp-widget__modal-open";
+
+export const MOBILE_BREAKPOINT = "495px";
