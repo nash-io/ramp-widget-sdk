175e3dc2fa8e7f9b7cf96f9cf1f74791ed4978be
Mateus Ferreira <mateus@dlbn.co>
2021-04-30 17:25:38 -0300

    chore(build): generate type declarations

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,15 +1,1 @@
-export interface FiatRampRate {
-    name: string;
-    minPurchaseAmount: {
-        amount: number;
-        currency: string;
-    };
-    purchaseFiatPrice: {
-        price: {
-            amount: Number;
-            currencyA: string;
-            currencyB: string;
-        };
-    };
-}
 export declare type WidgetEnvironment = "LOCAL" | "PRODUCTION";
---
+++ dist/utils.d.ts
@@ -0,0 +1,3 @@
+import { GetStylesProps } from "./assets/styles";
+export declare const stringifyQuery: (params: Record<string, any>) => string;
+export declare const setCustomVh: (styleOptions: GetStylesProps) => void;
