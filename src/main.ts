c40308e7c51176e24da3067db16f80ccd2ff2e85
Mateus Dal Bianco <mateus@dlbn.co>
2023-11-02 16:05:45 +0100

    docs: update JSDoc

--- src/main.ts
+++ src/main.ts
@@ -65,9 +65,12 @@ export default class NashRamp {
     return `${origin}?${query}`;
   }
   /**
-   * @param  {{width:number;height:number}} options
+   * @param  {{width:number|string;height:number|string;baseAmount:number|string;targetAmount:number|string;mode:`BUY`|`SELL`;}} options
    * @param  {number|string} options.width - Element width (e.g. "100%"; 320; "320px")
    * @param  {number|string} options.height - Element width (e.g. "100%"; 480; "480px")
+   * @param  {number|string} options.baseAmount - Initializes the widget with a fixed base amount.
+   * @param  {number|string} options.targetAmount - Initializes the widget with a fixed target amount.
+   * @param  {`BUY`|`SELL`} options.mode - Initializes the widget on Buy or Sell mode.
    */
   init(options: {
     width: number | string;
